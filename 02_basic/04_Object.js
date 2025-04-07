74d5da5fd97592c147c8e8f47f32e158e0aac61d
Aman Kumar <98329171+1Amankumar6@users.noreply.github.com>
2025-04-07 11:33:05 +0000

    object2 update

---
+++ 02_basic/04_Object.js
@@ -0,0 +1,19 @@
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname?.userfullname?.firstname+" "+regularUser.fullname?.userfullname?.lastname);
